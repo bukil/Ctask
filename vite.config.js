425e0652102a993415ed5baa23375e227766ffa3
MUKIL KUMAR <42886243+bukil@users.noreply.github.com>
2025-11-26 23:04:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  // Base path for GitHub Pages. Update if your repo name changes.
-  base: '/Ctask/',
   plugins: [react()],
+  base: '/Ctask/'   // must match the repo name INCLUDING capital C
 })
